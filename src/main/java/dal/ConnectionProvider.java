448901f801f35613f8bcfb6c2b9cbabd064ae919
tprivat2023 <tprivat2023@129SE23-7D4MMS3.ad.campus-eni.fr>
2023-09-04 14:17:45 +0200

    Mise en place finale du projet

---
+++ src/main/java/dal/ConnectionProvider.java
@@ -0,0 +1,50 @@
+package dal;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class ConnectionProvider {
+
+private static DataSource dataSource;
+	
+	/*
+	 * bloc static :
+	 * bloc de code qui s'éxécute une fois et une seule fois,
+	 * la 1ere fois que l'on appelle la classe (soit par instanciation (new()), 
+	 * soit par appel de méthode static (public), 
+	 * soit par appel d'attribut static (public)).
+	 */
+	static {
+		
+		try {
+			//1. récupérer les infos du fichier context.xml
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/pool_cnx");
+			Connection cnx = dataSource.getConnection();
+			
+			System.out.println("La connexion est : " + 
+							(cnx.isClosed() ? " fermée" : "ouverte"));
+			
+			//3. libère la connexion
+			cnx.close();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Connection getConnection() throws SQLException {
+		//2. récupérer la connexion à la BDD
+		return dataSource.getConnection();
+	}
+	
+	
+	
+}
