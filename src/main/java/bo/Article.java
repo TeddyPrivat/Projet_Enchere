857c0657de5ec6af653cae714b07bba5083ae1f4
tprivat2023 <tprivat2023@129SE23-7D4MMS3.ad.campus-eni.fr>
2023-09-05 11:48:55 +0200

    commit

---
+++ src/main/java/bo/Article.java
@@ -0,0 +1,135 @@
+package bo;
+
+import java.time.LocalDate;
+
+public class Article {
+
+	private int noArticle;
+	private String nomArticle;
+	private String description;
+	private LocalDate dateDebutEncheres;
+	private LocalDate dateFinEncheres;
+	private int miseAPrix;
+	private int prixVente;
+	
+	public enum Etat{
+		A_VENDRE("A vendre"),
+		EN_VENTE("En vente"),
+		VENDU("Vendu");
+		
+		private String etat;
+		private Etat(String etat) {
+			this.etat = etat;
+		}
+		
+		public String getEtat() {
+			return this.etat;
+		}
+	}
+	private Etat etatVente;
+	
+	/*
+	private Utilisateur utilisateur; 
+	private Retrait retrait;
+	private Categorie categorie;
+	private Enchere enchere;*/
+	
+	
+	public Article(int noArticle, String nomArticle, String description, LocalDate dateDebutEncheres,
+			LocalDate dateFinEncheres, int miseAPrix, int prixVente, Etat etatVente) {
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEncheres = dateDebutEncheres;
+		this.dateFinEncheres = dateFinEncheres;
+		this.miseAPrix = miseAPrix;
+		this.prixVente = prixVente;
+		this.etatVente = etatVente;
+	}
+	
+	public Article(Etat etatVente) {
+		this.etatVente = etatVente;
+	}
+	
+	public int getNoArticle() {
+		return noArticle;
+	}
+	
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+	public String getNomArticle() {
+		return nomArticle;
+	}
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public LocalDate getDateDebutEncheres() {
+		return dateDebutEncheres;
+	}
+	public void setDateDebutEncheres(LocalDate dateDebutEncheres) {
+		this.dateDebutEncheres = dateDebutEncheres;
+	}
+	public LocalDate getDateFinEncheres() {
+		return dateFinEncheres;
+	}
+	public void setDateFinEncheres(LocalDate dateFinEncheres) {
+		this.dateFinEncheres = dateFinEncheres;
+	}
+	public int getMiseAPrix() {
+		return miseAPrix;
+	}
+	public void setMiseAPrix(int miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+	public int getPrixVente() {
+		return prixVente;
+	}
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+	public String getEtatVente() {
+		return etatVente.getEtat();
+	}
+	public void setEtatVente(Etat etatVente) {
+		this.etatVente = etatVente;
+	}
+	/*
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	public Retrait getRetrait() {
+		return retrait;
+	}
+	public void setRetrait(Retrait retrait) {
+		this.retrait = retrait;
+	}
+	public Categorie getCategorie() {
+		return categorie;
+	}
+	public void setCategorie(Categorie categorie) {
+		this.categorie = categorie;
+	}
+	public Enchere getEnchere() {
+		return enchere;
+	}
+	public void setEnchere(Enchere enchere) {
+		this.enchere = enchere;
+	}*/
+	@Override
+	public String toString() {
+		return "Article [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEncheres=" + dateDebutEncheres + ", dateFinEncheres=" + dateFinEncheres + ", miseAPrix="
+				+ miseAPrix + ", prixVente=" + prixVente + ", etatVente=" + etatVente.getEtat() + "]";
+	}
+
+}
