048135e158c1bc3dc72cacc0eaac3dc8734016f0
zabesadze2023 <zabesadze2023@129SE23-DD4MMS3.ad.campus-eni.fr>
2023-09-06 16:30:08 +0200

    Inscription à verifier

---
+++ src/main/java/fr/eni/javaee/enchere/servlets/ServletInscription.java
@@ -0,0 +1,29 @@
+package fr.eni.javaee.enchere.servlets;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class ServletInscription
+ */
+public class ServletInscription extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/inscription.jsp");
+		rd.forward(request, response);
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
