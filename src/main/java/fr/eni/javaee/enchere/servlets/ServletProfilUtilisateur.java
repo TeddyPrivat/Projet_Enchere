8ef59eaf3df69d60feb90bd96e4603d77ed9ae0c
tprivat2023 <tprivat2023@129SE23-7D4MMS3.ad.campus-eni.fr>
2023-09-08 14:38:00 +0200

    save

--- src/main/java/fr/eni/javaee/enchere/servlets/ServletProfilUtilisateur.java
+++ src/main/java/fr/eni/javaee/enchere/servlets/ServletProfilUtilisateur.java
@@ -16,13 +16,18 @@ public class ServletProfilUtilisateur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getParameter("noUtilisateur"); //récupère l'ID du vendeur
+		int noUtilisateur = Integer.valueOf(request.getParameter("noUtilisateur"));
+		//System.out.println(noUtilisateur);
 		
+		Utilisateur utilisateur = UtilisateurManager.getInstance().selectInfoUtilisateur(noUtilisateur);
+		/*
 		Utilisateur utilisateur = UtilisateurManager.getInstance().selectInfoUtilisateur(2);
 		int iduser1 = UtilisateurManager.getInstance().selectByIdentifiant(utilisateur.getPseudo(),utilisateur.getMotDePasse());
 		utilisateur = UtilisateurManager.getInstance().selectInfoUtilisateur(iduser1);
 		System.out.println(iduser1+"id");
 		System.out.println("coucou"+utilisateur);
-		request.setAttribute("utilisateur", utilisateur);
+		request.setAttribute("utilisateur", utilisateur);*/
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/profilutilisateur.jsp");
 		rd.forward(request, response);
 		
